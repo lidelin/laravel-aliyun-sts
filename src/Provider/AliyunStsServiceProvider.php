d41a68f757633a5bf69ff159c739703dec2fef88
lidelin <lidelin1023@qq.com>
2017-10-19 10:37:27 +0800

    first commit

---
+++ src/Provider/AliyunStsServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LDL\Aliyun\Sts\Provider;
+
+use RuntimeException;
+use Illuminate\Support\ServiceProvider;
+
+class AliyunStsServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $this->publishes([
+            realpath(__DIR__ . '/../../config/aliyun_sts.php') => config_path('aliyun_sts.php'),
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(realpath(__DIR__ . '/../../config/aliyun_sts.php'), 'aliyun_sts');
+
+        $config = $this->app['config']['aliyun_sts'];
+
+        if (!$this->app->runningInConsole() && (empty($config['key']) || empty($config['secret']) || empty($config['role_arn']))) {
+            throw new RuntimeException('Unable to boot AliyunStsServiceProvider, please set key, secret, role_arn in `config/aliyun_sts.php` file.');
+        }
+    }
+}
